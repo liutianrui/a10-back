c755a7c277f4cdd36041329e920d8a8da31631ec
gpf12344 <1148425751@qq.com>
2023-04-28 13:19:40 +0800

    封装冗余代码，动态测试

--- Rf_Classifier_Plus.py
+++ Rf_Classifier_Plus.py
@@ -17,9 +17,7 @@ import pandas as pd
 # from sklearn.model_selection import GridSearchCV, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 
-
-def classify(filepath):
-    data = filepath
+def importData(data):
     Fault_diagnosis_data = pd.read_csv(data,
                                        usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
                                                 20,
@@ -32,6 +30,57 @@ def classify(filepath):
                                                 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
                                                 96,
                                                 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108])
+    return Fault_diagnosis_data
+
+def data_process(Fault_diagnosis_data,feature_mean,feature_median,feature_mode):
+    length1 = len(feature_mean)
+    i = 0
+    while i < length1:
+        Fault_diagnosis_data[feature_mean[i]].fillna(Fault_diagnosis_data[feature_mean[i]].mean(),
+                                                     inplace=True)  # 使用该列平均值进行空缺值替换
+        i += 1
+
+    length2 = len(feature_median)
+    j = 0
+    while j < length2:
+        Fault_diagnosis_data[feature_median[j]].fillna(Fault_diagnosis_data[feature_median[j]].median(), inplace=True)
+        j += 1
+
+    length3 = len(feature_mode)
+    k = 0
+    while k < length3:
+        Fault_diagnosis_data[feature_mode[k]].fillna(Fault_diagnosis_data[feature_mode[k]].mode().iloc[0], inplace=True)
+        k += 1
+
+def loadData(Fault_diagnosis_data):
+    # 载入特征和标签集
+    X_test = Fault_diagnosis_data[
+        ['feature0', 'feature1', 'feature2', 'feature3', 'feature4', 'feature5', 'feature6', 'feature7', 'feature8',
+         'feature9', 'feature10', 'feature11', 'feature12', 'feature13', 'feature14', 'feature15', 'feature16',
+         'feature17',
+         'feature18', 'feature19', 'feature20', 'feature21', 'feature22', 'feature23', 'feature24', 'feature25',
+         'feature26', 'feature27', 'feature28', 'feature29', 'feature30', 'feature31', 'feature32', 'feature33',
+         'feature34', 'feature35', 'feature36', 'feature37', 'feature38', 'feature39', 'feature40', 'feature41',
+         'feature42', 'feature43', 'feature44', 'feature45', 'feature46', 'feature47', 'feature48', 'feature49',
+         'feature50', 'feature51', 'feature52', 'feature53', 'feature54', 'feature55', 'feature56', 'feature57',
+         'feature58', 'feature59', 'feature60', 'feature61', 'feature62', 'feature63', 'feature64', 'feature65',
+         'feature66', 'feature67', 'feature68', 'feature69', 'feature70', 'feature71', 'feature72', 'feature73',
+         'feature74', 'feature75', 'feature76', 'feature77', 'feature78', 'feature79', 'feature80', 'feature81',
+         'feature82', 'feature83', 'feature84', 'feature85', 'feature86', 'feature87', 'feature88', 'feature89',
+         'feature90', 'feature91', 'feature92', 'feature93', 'feature94', 'feature95', 'feature96', 'feature97',
+         'feature98', 'feature99', 'feature100', 'feature101', 'feature102', 'feature103', 'feature104', 'feature105',
+         'feature106']]
+    y_test = Fault_diagnosis_data['label']
+    
+    return X_test,y_test
+
+def classify(filepath):
+    data_test = filepath
+    data_train = "E:SoftCup\preprocess_train.csv"
+
+    #数据读取
+    Fault_diagnosis_data_train = importData(data_train)
+    Fault_diagnosis_data_test = importData(data_test)
 
     feature_mean = ['feature0', 'feature2', 'feature4', 'feature7', 'feature8', 'feature11', 'feature15', 'feature22',
                     'feature23',
@@ -56,67 +105,31 @@ def classify(filepath):
                     'feature78',
                     'feature80',
                     'feature88', 'feature92']
-
-    # 1.
-    print("初始feature0列空值数量：", Fault_diagnosis_data['feature0'].isna().sum())  # 查看未处理的feature0列空值数量
-    length1 = len(feature_mean)
-    i = 0
-    while i < length1:
-        Fault_diagnosis_data[feature_mean[i]].fillna(Fault_diagnosis_data[feature_mean[i]].mean(),
-                                                     inplace=True)  # 使用该列平均值进行空缺值替换
-        i += 1
-    print(Fault_diagnosis_data['feature0'].isna().sum())  # 查看使用平均值替换处理后的feature0列空值数量
-
-    # 2.
-    print("初始feature3列空值数量：", Fault_diagnosis_data['feature3'].isna().sum())  # 查看未处理的feature0列空值数量
-    length2 = len(feature_median)
-    j = 0
-    while j < length2:
-        Fault_diagnosis_data[feature_median[j]].fillna(Fault_diagnosis_data[feature_median[j]].median(), inplace=True)
-        j += 1
-    print(Fault_diagnosis_data['feature3'].isna().sum())  # 查看使用平均值替换处理后的feature0列空值数量
-
-    # 3.
-    print("初始feature1列空值数量：", Fault_diagnosis_data['feature1'].isna().sum())  # 查看未处理的feature0列空值数量
-    length3 = len(feature_mode)
-    k = 0
-    while k < length3:
-        Fault_diagnosis_data[feature_mode[k]].fillna(Fault_diagnosis_data[feature_mode[k]].mode().iloc[0], inplace=True)
-        k += 1
-    print(Fault_diagnosis_data['feature1'].isna().sum())  # 查看使用平均值替换处理后的feature0列空值数量
+    
+    # 数据处理
+    data_process(Fault_diagnosis_data_train,feature_mean,feature_median,feature_mode)
+    data_process(Fault_diagnosis_data_test,feature_mean,feature_median,feature_mode)
 
     # 载入特征和标签集
-    X = Fault_diagnosis_data[
-        ['feature0', 'feature1', 'feature2', 'feature3', 'feature4', 'feature5', 'feature6', 'feature7', 'feature8',
-         'feature9', 'feature10', 'feature11', 'feature12', 'feature13', 'feature14', 'feature15', 'feature16',
-         'feature17',
-         'feature18', 'feature19', 'feature20', 'feature21', 'feature22', 'feature23', 'feature24', 'feature25',
-         'feature26', 'feature27', 'feature28', 'feature29', 'feature30', 'feature31', 'feature32', 'feature33',
-         'feature34', 'feature35', 'feature36', 'feature37', 'feature38', 'feature39', 'feature40', 'feature41',
-         'feature42', 'feature43', 'feature44', 'feature45', 'feature46', 'feature47', 'feature48', 'feature49',
-         'feature50', 'feature51', 'feature52', 'feature53', 'feature54', 'feature55', 'feature56', 'feature57',
-         'feature58', 'feature59', 'feature60', 'feature61', 'feature62', 'feature63', 'feature64', 'feature65',
-         'feature66', 'feature67', 'feature68', 'feature69', 'feature70', 'feature71', 'feature72', 'feature73',
-         'feature74', 'feature75', 'feature76', 'feature77', 'feature78', 'feature79', 'feature80', 'feature81',
-         'feature82', 'feature83', 'feature84', 'feature85', 'feature86', 'feature87', 'feature88', 'feature89',
-         'feature90', 'feature91', 'feature92', 'feature93', 'feature94', 'feature95', 'feature96', 'feature97',
-         'feature98', 'feature99', 'feature100', 'feature101', 'feature102', 'feature103', 'feature104', 'feature105',
-         'feature106']]
-    y = Fault_diagnosis_data['label']
+    X_train,y_train = loadData(Fault_diagnosis_data_train)
+    X_test,y_test = loadData(Fault_diagnosis_data_test)
 
-    X_rf_train = X[:5600]
-    y_rf_train = y[:5600]
+
+    #取训练集
+    X_rf_train = X_train
+    y_rf_train = y_train
+    #    X_rf_train = X_test
 
     # 取测试集
-    X_rf_test = X[5600:6296]
-    y_rf_test = y[5600:6296]
+    X_rf_test = X_test
+    y_rf_test = y_test
 
     # 建立随机森林模型
     rfc = RandomForestClassifier(n_estimators=281, criterion='gini', max_depth=18, max_features=107, random_state=100)
     rfc.fit(X_rf_train, y_rf_train)  # 模型训练
     y_rf_pred = rfc.predict(X_rf_test)  # 使用随机森林（281个决策树分类器）多数投票产生的预测label列表(测试集改为1000条样本)
 
-    # length_train = len(X)
+    # length_train = len(X_test)
     length_test = len(y_rf_test)  # 500
 
     TP = 0
